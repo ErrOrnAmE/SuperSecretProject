e4d5e701334923979cf0218d67b6b87d242e439a
Prygan <prygan@gmail.com>
2015-05-29 18:14:39 +0200

    drfe

--- interpretation/interpretation.py
+++ interpretation/interpretation.py
@@ -13,16 +13,11 @@ class Interpretation:
         self.scoreNous=0
         self.scoreAdvers=0
         self.tablier = []
-        self.tabLock = []
+        self.tablock = []
 
     def routeur(self, msg):
         num = msg[0]+msg[1]
-        
-        if(num == '1-'):
-            1+0
-        elif(num == '2-'):
-            1+0
-        elif(num == '01'):
+        if(num == '01'):
             self.generateTablier(msg.split("=")[1])
         elif(num == '10'):
             ia.coupOptimal(self.tablier)
@@ -30,10 +25,16 @@ class Interpretation:
 
     def generateTablier(self, msg):
         lineArray = msg.split("|")
+        cbLigne = len(lineArray)
+        cbColonne = len(lineArray[0].split(":"))
         for line  in lineArray:
-                self.tablier.append(line.split(":"))
-                
+            self.tablier.append(line.split(":"))
+        for x in range(0,cbColonne):
+            foo = []
+            for y in range(0,cbLigne):
+                foo.append(0)
+            self.tablock.append(foo)
         
-    
+
     def majLock(x,y,who): 
         self.tablock[x][y] = who
