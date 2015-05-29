6d0738dd644850052bc08b8c8f94d5006cdb8295
ErrOrnAmE <ErrOrnAmE@github.com>
2015-05-29 16:17:05 +0200

    Correct pour lock 0/0

--- ia/ia.py
+++ ia/ia.py
@@ -3,7 +3,10 @@
 
 tablier = [[1,2,3],[4,5,6],[7,8,9]]
 
-tablock = [[-1,1,0,0],[-1,0,0,0],[0,0,0,0],[0,0,0,0]]
+tablock = [[-1,1,0,0],[0,-1,0,0],[0,0,0,0],[0,0,0,0]]
+
+largeur = 3
+hauteur = 3
 
 class IA(object):
 
@@ -12,7 +15,7 @@ class IA(object):
 		self.tablock = tablock;
 
 	def createMap(self):
-		self.lock = Lock(self.tablier,self.tablock,1,1)
+		self.lock = Lock(self.tablier,self.tablock,0,1)
 
 
 
@@ -20,20 +23,50 @@ class IA(object):
 class Lock(object):
 
 	def __init__(self,tablier,tablock,x,y):
-		conteneurs = tablier[x-1:x+1]
-		for index in range(len(conteneurs)):
-			conteneurs[index] = conteneurs[index][y-1:y+1]
+		self.x = x
+		self.y = y
 
-		locks = tablock[x-1:x+2]
-		for index in range(len(locks)):
-			locks[index] = locks[index][y-1:y+2]
+		if (tablock[x][y] != 0)
+			self.diff = None
+			return
 
-		#print(conteneurs)
+		if (x > 0 and y > 0 and x < largeur and y < hauteur):
+			conteneurs = tablier[x-1:x+1]
+			for index in range(len(conteneurs)):
+				conteneurs[index] = conteneurs[index][y-1:y+1]
 
-		avant = self.nbPointsLock(conteneurs,locks)
-		locks[1][1] = 1
-		#print(locks)
-		apres = self.nbPointsLock(conteneurs,locks)
+			print(conteneurs)
+
+			locks = tablock[x-1:x+2]
+			for index in range(len(locks)):
+				locks[index] = locks[index][y-1:y+2]
+
+			#print(conteneurs)
+
+			avant = self.nbPointsLock(conteneurs,locks)
+			locks[1][1] = 1
+			#print(locks)
+			apres = self.nbPointsLock(conteneurs,locks)
+
+			#print("nous: "+str(apres[0]-avant[0])+" eux:"+str(apres[1]-avant[1]) )
+			self.diff = (apres[0]-avant[0])-(apres[1]-avant[1])
+		elif (x == 0 and y == 0):
+			minilocks = tablock[0:2]
+			for index in range(len(minilocks)):
+				minilocks[index] = minilocks[index][0:2]
+
+			print(minilocks)
+
+			euxLocks = self.nbLocks(minilocks,-1)
+			nousLocks = self.nbLocks(minilocks,1)
+
+			if (euxLocks == nousLocks or euxLocks == nousLocks + 1):
+				self.diff = tablier[0][0]
+			else:
+				self.diff = 0
+		
+
+		print(str(x)+":"+str(y)+" > "+str(self.diff))
 
 	def nbPointsLock(self,conteneurs,locks):
 		nous = 0
@@ -52,9 +85,7 @@ class Lock(object):
 				elif (nousLocks > euxLocks):
 					nous = nous + conteneurs[x][y]
 
-		print("score:")
-		print(nous)
-		print(eux)
+		return (nous,eux)
 
 	def nbLocks(self,locks,value):
 		nb = 0
