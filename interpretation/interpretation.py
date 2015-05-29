7e61625f3b61b824c31627ead8d7bfb27ae2f44e
Prygan <prygan@gmail.com>
2015-05-29 16:04:28 +0200

    drfe

--- interpretation/interpretation.py
+++ interpretation/interpretation.py
@@ -1,3 +1,19 @@
 class Interpretation:
+    """
+    def generateTablier(self, msg):
+       
+        self.tablier
+        self.tablock
+       
+    """     
+
+    def generateTablier(self, msg):
+        self.tablier = []
+        lineArray = msg.split("|")
+        for line  in lineArray:
+                self.tablier.append(line.split(":"))
+        return self.tablier
+
+    
 
     
