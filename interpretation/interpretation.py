718b13a5b41efd5ecb07b7e8e77fb965264577ab
Prygan <prygan@gmail.com>
2015-05-29 18:58:23 +0200

    fereffe

--- interpretation/interpretation.py
+++ interpretation/interpretation.py
@@ -16,14 +16,29 @@ class Interpretation:
         self.tablock = []
 
     def routeur(self, msg):
-        num = msg[0]+msg[1]
-        if(num == '01'):
-            self.generateTablier(msg.split("=")[1])
-        elif(num == '10'):
-            ia.coupOptimal(self.tablier)
+        msgArray = msg.split("\n")
+        for mess in msgArray:
+            num = mess[0]+mess[1]
+            if(num == 'MA'):
+                self.generateTablier(mess.split("=")[1])
+            elif(num == '10'):
+                IA(self.tablier, self.tablock).getCoordBestLock()
+            elif(num == '20'):
+                shit = mess.split(":")[2]
+                coord = self.shitToCoord(shit)
+                self.majLock(coord[0],coord[1], 1)
+            elif(num == '88'):
+                res = msg.split(" ")[4] 
+                if res == ("gagné"): 
+                    return "win" 
+                else:
+                    return "lose"
+            return ""
 
 
     def generateTablier(self, msg):
+        if msg[len(msg)-1] == "|":
+            msg = msg[:len(msg)-1]
         lineArray = msg.split("|")
         cbLigne = len(lineArray)
         cbColonne = len(lineArray[0].split(":"))
