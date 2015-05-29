572f0db355e00d6183892ef479dab26cfa9307ca
Prygan <prygan@gmail.com>
2015-05-29 19:26:49 +0200

    efushgf

--- interpretation/interpretation.py
+++ interpretation/interpretation.py
@@ -1,3 +1,5 @@
+from ia import IA
+
 class Interpretation:
     """
     def generateTablier(self, msg):
@@ -8,10 +10,6 @@ class Interpretation:
     """     
 
     def __init__(self):
-        self.nbJetons=20
-        self.nbJetonsAdvers=20
-        self.scoreNous=0
-        self.scoreAdvers=0
         self.tablier = []
         self.tablock = []
 
@@ -19,13 +17,15 @@ class Interpretation:
         msgArray = msg.split("\n")
         for mess in msgArray:
             num = mess[0]+mess[1]
+            print(num)
             if(num == 'MA'):
                 self.generateTablier(mess.split("=")[1])
             elif(num == '10'):
-                IA(self.tablier, self.tablock).getCoordBestLock()
+                coord = IA(self.tablier, self.tablock).getCoordBestLock()
+                return self.cord2Shit(coord)
             elif(num == '20'):
                 shit = mess.split(":")[2]
-                coord = self.shitToCoord(shit)
+                coord = self.shit2Cord(shit)
                 self.majLock(coord[0],coord[1], 1)
             elif(num == '88'):
                 res = msg.split(" ")[4] 
@@ -33,7 +33,38 @@ class Interpretation:
                     return "win" 
                 else:
                     return "lose"
-            return ""
+        return ""
+
+    def shit2Cord(self,string): 
+        taille = len(string) 
+        if taille==3: 
+            x = (int) (string[0])-1 
+            y = ord(string[1]) - 65
+            coté = (int) (string[2]) 
+        elif taille==4: 
+            x = int (string[0]+string[1])-1 
+            y = ord(string[2]) -65
+            coté = (int) (string[3]) 
+        if coté ==1: 
+            return (x,y) 
+        elif coté == 2: 
+            return (x+1,y) 
+        elif coté == 3: 
+            return (x+1,y+1) 
+        elif coté == 4: 
+            return (x,y+1)
+
+    def cord2Shit(self,x,y): 
+        longeur = len (self.tablock) 
+        largeur = len (self.tablock[0]) 
+        if y < longeur and x < largeur: 
+            return str(x)+chr(y+65)+"1" 
+        elif y== longeur and x == largeur: 
+            return str(x-1)+chr((y-1)+65)+"3" 
+        elif y == longeur: 
+            return str(x)+chr((y-1)+65)+"4" 
+        elif x== largeur: 
+            return str(x-1)+chr((y)+65)+"2"
 
 
     def generateTablier(self, msg):
